11d1b612327e13163059313cbf81c3e38f03962b
delosHoyos <delos_16@hotmail.com>
2016-05-16 11:02:09 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,1 @@
 // JavaScript Document
-
-window.addEventListener("load", arrancar, false);
-
-function arrancar() {
-	
-	$.getJSON("http://localhost/app/listaItems.php", function(datos){
-	/*Primer parametro: url a la que le hago la peticion - Segundo: si queres mandar un dato al server - Tercer: Funcion*/		
-	
-	alert ("nice");	
-	
-	});
-    
-}
